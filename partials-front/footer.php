e93f398f41ccd1f2dbd756b220b472e8458feba9
Ananta Nath <nathananta0001@gmail.com>
2022-11-16 21:12:42 +0530

    added css

---
+++ partials-front/footer.php
@@ -0,0 +1,12 @@
+
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>Project by Ananta Nath, Harshita Singhal, Shreya Ranjan, Asif Hussain</p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
